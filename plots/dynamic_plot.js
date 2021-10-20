331445fbaf0751f10ddc739cca5303fc9b6c56a5
Christian Rutzer <rutzer@service06.cluster.bc2.ch>
2021-10-20 14:31:27 +0200

    plots added

---
+++ plots/dynamic_plot.js
@@ -0,0 +1,84 @@
+d3.csv("https://raw.githubusercontent.com/ch54321/mrna_tech/master/general_trend_mrna.csv", function(err, rows){
+
+  function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+}
+
+  var frames = [];
+  var x = unpack(rows, 'filing');
+  var y = unpack(rows, 'value');
+
+  var n = 11;
+  for (var i = 0; i < n; i++) {
+    frames[i] = {data: [{x: [], y: []}]};
+    frames[i].data[0].x = x.slice(0, i+1);
+    frames[i].data[0].y = y.slice(0, i+1);
+  }
+
+  Plotly.newPlot('myDiv', [{
+    x: frames[1].data[0].x,
+    y: frames[1].data[0].y,
+    fill: 'tozeroy',
+    type: 'scatter',
+    mode: 'lines',
+    line: {color: 'green'}
+  }], {
+    title: "Filled-Area Animation",
+    xaxis: {
+      type: 'date',
+      range: [
+        frames[10].data[0].x[0],
+        frames[10].data[0].x[10]
+      ]
+    },
+    yaxis: {
+      range: [
+        0,
+        0.02
+      ]
+    },
+    updatemenus: [{
+      x: 0.1,
+      y: 0,
+      yanchor: "top",
+      xanchor: "right",
+      showactive: false,
+      direction: "left",
+      type: "buttons",
+      pad: {"t": 87, "r": 10},
+      buttons: [{
+        method: "animate",
+        args: [null, {
+          fromcurrent: true,
+          transition: {
+            duration: 0,
+          },
+          frame: {
+            duration: 250,
+            redraw: false
+          }
+        }],
+        label: "Play"
+      }, {
+        method: "animate",
+        args: [
+          [null],
+          {
+            mode: "immediate",
+            transition: {
+              duration: 0
+            },
+            frame: {
+              duration: 0,
+              redraw: false
+            }
+          }
+        ],
+        label: "Pause"
+      }]
+    }]
+  }).then(function() {
+    Plotly.addFrames('myDiv', frames);
+  });
+
+});
